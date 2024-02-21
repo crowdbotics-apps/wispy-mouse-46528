a67201070b2528fee7c96b87d5f0418faa9e9cd6
Crowdbotics <team@crowdbotics.com>
2024-02-21 14:33:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-mouse-46528",
+  url: "https://wispy-mouse-46528.botics.co",
+  api: "https://wispy-mouse-46528.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
